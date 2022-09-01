a6b1d798f0f6f88d036a50510caa26e808169e78
AlfikaNM <alfikanmuis@gmail.com>
2022-09-01 17:34:02 +0700

    add API-endpoint

--- src/main/java/com/day13/apachePoiDemo/controller/CryptocurrencyController.java
+++ src/main/java/com/day13/apachePoiDemo/controller/CryptocurrencyController.java
@@ -1,4 +1,21 @@
 package com.day13.apachePoiDemo.controller;
 
+import com.day13.apachePoiDemo.service.CryptocurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/crypto")
 public class CryptocurrencyController {
+
+    @Autowired
+    private CryptocurrencyService cryptocurrencyService;
+
+    @PostMapping(path = "/import-data")
+    public  void  importDataFromExcelToDb(@RequestPart(required = true)List<MultipartFile> files){
+        cryptocurrencyService.importToDb(files);
+    }
 }
